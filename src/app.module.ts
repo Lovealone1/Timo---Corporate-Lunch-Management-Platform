fa386051d7c73b808b09c8e62cb95c07bd4aab4b
Dan <dgarcia232@soyudemedellin.edu.co>
2026-02-20 12:22:38 -0500

    feat: :sparkles: add protein management module (create, list, get, deactivate, delete)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from './modules/auth/auth.module';
+import { ProteinsModule } from './modules/proteins/proteins.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
     }),
-    AuthModule
+    AuthModule,
+    ProteinsModule
   ],
 })
 export class AppModule { }
